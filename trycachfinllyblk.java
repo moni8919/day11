67d95dfd81a6856afaa45a21da7fb3bf3713cfb6
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-14 15:06:46 +0530

    Add files via upload

---
+++ trycachfinllyblk.java
@@ -0,0 +1,13 @@
+public class trycachfinllyblk {
+    public static void main(String[]args){
+        try{
+            throw new Exception("error");
+            
+        }catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+       
+    }
+    
+}
