d0b187c1ac9658468e3df0c7c3739d1eeadf3e47
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-14 15:07:50 +0530

    Add files via upload

---
+++ main2.java
@@ -0,0 +1,8 @@
+  import java.util.*;
+  public class main2 {
+    public static void main(String[] args)throws ArithmeticException{
+    int a=10;
+    int b=0;
+    System.out.println(a/b);
+    }
+}
